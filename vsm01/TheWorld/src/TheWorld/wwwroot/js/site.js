9730a278d422ef7c1472f621a69a6c63a627eb72
gcroteau <guillaume.croteau.1@gmail.com>
2016-02-17 16:33:04 -0500

    In module 3

---
+++ vsm01/TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    //var ele = document.getElementById("username");
+    //ele.innerHTML = "User name";
+
+    var main = document.getElementById("main");
+
+    main.onmouseenter = function () {
+        main.style = "background-color: #888;";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+})();
