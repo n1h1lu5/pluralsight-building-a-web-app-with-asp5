64e4b9cd170b4085cd6010297040535cc6e5c490
gcroteau <guillaume.croteau.1@gmail.com>
2016-02-18 14:25:46 -0500

    Completed module 3

--- vsm01/TheWorld/src/TheWorld/wwwroot/js/site.js
+++ vsm01/TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -2,13 +2,30 @@
     //var ele = document.getElementById("username");
     //ele.innerHTML = "User name";
 
-    var main = document.getElementById("main");
+    //var main = $("#main");
+    //
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //
+    //main.on("mouseleave",function () {
+    //    main.style = "";
+    //});
+    //
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //
+    //});
 
-    main.onmouseenter = function () {
-        main.style = "background-color: #888;";
-    };
+    var sidebarAndWrapper = $("#sidebar,#wrapper");
 
-    main.onmouseleave = function () {
-        main.style = "";
-    };
+    $("#sidebarToggle").on("click", function () {
+        sidebarAndWrapper.toggleClass("hide-sidebar");
+        if (sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
 })();
