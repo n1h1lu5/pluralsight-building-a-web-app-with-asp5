8506b9103fe1ff641ed6a4295eba27ba2330851c
gcroteau <guillaume.croteau.1@gmail.com>
2016-02-23 16:59:51 -0500

    In module 9

---
+++ vsm01/TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+
+        vm.name = "My name";
+
+        vm.trips = [
+            {
+                name: "us trip",
+                created: new Date()
+            },
+            {
+                name: "a trip",
+                create: new Date()
+            }
+        ]
+    }
+})();
